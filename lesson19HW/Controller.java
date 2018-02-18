36e17d6e05c0aeabdcd997213f572f7491a01100
Miralex <drive_forever@mail.ru>
2018-02-18 02:11:52 +0200

    lesson19HW

--- lesson19HW/Controller.java
+++ lesson19HW/Controller.java
@@ -2,17 +2,20 @@ package lesson19HW;
 
 public class Controller {
 
-    public void put(Storage storage, File file){
+    public void put(Storage storage, File file) throws Exception{
+
 
     }
 
-    public void delete(Storage storage, File file){
+    public void delete(Storage storage, File file) throws Exception{
 
     }
-    public void transferAll(Storage storageFrom, Storage storageTo){
+
+    public void transferAll(Storage storageFrom, Storage storageTo) throws Exception{
 
     }
-    public void transferFile(Storage storageFrom, Storage storageTo, long id){
+
+    public void transferFile(Storage storageFrom, Storage storageTo, long id) throws Exception{
 
     }
 }
