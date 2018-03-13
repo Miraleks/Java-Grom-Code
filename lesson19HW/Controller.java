5af71fd1052fac02fa861ffbdb4f22157bbcef5e
Alex Mironositskiy <drive_forever@mail.ru>
2018-03-13 08:47:52 +0200

    lesson19HW

--- lesson19HW/Controller.java
+++ lesson19HW/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
         for(int i = 0; i < files.length; i++){
             if(files[i] == null) {
                 files[i] = file;
+                storage.setFiles(files);
                 break;
             }
         }
