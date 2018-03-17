99aa74e3cb8edac83f19292f3bfc4bbb8a88dda4
Miralex <drive_forever@mail.ru>
2018-03-17 18:19:17 +0200

    lesson20HW

--- lesson20/task2/Transaction.java
+++ lesson20/task2/Transaction.java
@@ -10,6 +10,30 @@ public class Transaction {
     private TransactionType type;
     private Date dateCreated;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (id != that.id) return false;
+        if (amount != that.amount) return false;
+        if (city != null ? !city.equals(that.city) : that.city != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        return type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + amount;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+
     public long getId() {
         return id;
     }
