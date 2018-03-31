942e0768d81af018becb1cb7fe00bcfe8babcf92
Miralex <drive_forever@mail.ru>
2018-04-01 02:17:24 +0300

    lesson27HW

---
+++ lesson27HW/Demo.java
@@ -0,0 +1,9 @@
+package lesson27HW;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedListTest linkedListTest = new LinkedListTest();
+
+        System.out.println(linkedListTest.useList());
+    }
+}
